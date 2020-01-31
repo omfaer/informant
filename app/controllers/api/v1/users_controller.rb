af4cda5729b44155ece01e9956f3702d9a40383b
omfaer <omfaer.ce@gmail.com>
2020-01-31 09:24:12 +0300

    Fix User creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 	before_action :get_user, only: [:show]
-	before_action :save_record, only: [:new, :create]
+	before_action :user_params, only: [:new, :create]
 
   def show
     render json: @user
@@ -11,12 +11,12 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create 
-  	@user = User.new(user_params)
+    @user = User.new(user_params)
   	    
     if @user.save
       render :status => "200", :json => {:message => "success"}.to_json
     else
-	  render :status => "503", :json => {:message => "service unavailable"}.to_json
+      render :status => "503", :json => {:message => "service unavailable"}.to_json
     end
    end
 
